1f060633aae1343863c2ba4a9b6f1da4aa1aa59a
Genwkwk <113852676+Genscy@users.noreply.github.com>
2022-11-14 12:08:50 +0800

    Add files via upload

---
+++ Dowhileifelse.java
@@ -0,0 +1,33 @@
+package dowhileifelse;
+
+import java.io.*;
+
+import java.util.Scanner;
+
+public class Dowhileifelse {
+
+            
+    public static void main(String[] args)throws InterruptedException{
+        char chr;
+        
+        do {
+            Scanner input = new Scanner(System.in);
+            
+            System.out.println("Enter Username: ");
+            String userName = input.nextLine();
+        
+            
+            System.out.println("Enter your Password: ");
+            String psw = input.nextLine();
+            
+            if (userName.equalsIgnoreCase("Genscy0903") && psw.equals("090903")) {
+                    System.out.println("Welcome, Master!");
+                    break;
+            } else {
+            System.out.println("Login Failed!");
+            } System.out.println("Continue? press Y for yes/ N for no");
+            chr = input.next().charAt(0);
+        } while(chr == 'y' || chr == 'Y');
+        
+    }
+}
---
+++ Joption.java
@@ -0,0 +1,31 @@
+package joption;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class Joption {
+
+    public static void main(String[] args) {
+       JOptionPane input = new JOptionPane("Laluan"); 
+       Random raa = new Random();
+       JOptionPane.showMessageDialog(null, "Hi, ");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    
+     int shown = 100;
+     while (true){
+     JDialog dialoghatdog = input.createDialog(null, "Virus!");
+     dialoghatdog.setLocation(raa.nextInt((int) screenSize.getWidth() - 50), raa.nextInt((int)screenSize.getHeight()) - 50);
+     dialoghatdog.setVisible(true);
+     
+     for(int i=0; i<=shown; i++){
+        JDialog a = input.createDialog(null, " Alert! Fix it immediately!");
+        a.setLocation(raa.nextInt((int) screenSize.getWidth() - 100), raa.nextInt((int) screenSize.getHeight()) - 100);
+        a.setModal(false);
+        a.setVisible(true);
+     }
+    shown++;
+     }
+    }
+}
+
